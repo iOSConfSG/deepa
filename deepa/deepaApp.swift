101b5e3d32d213b9cf507db86e08382801c9ca66
Vina Melody <vina_melody@yahoo.com>
2022-09-15 23:09:57 +0800

    Initial Commit

---
+++ deepa/deepaApp.swift
@@ -0,0 +1,17 @@
+//
+//  deepaApp.swift
+//  deepa
+//
+//  Created by Vina Melody on 15/9/22.
+//
+
+import SwiftUI
+
+@main
+struct deepaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
